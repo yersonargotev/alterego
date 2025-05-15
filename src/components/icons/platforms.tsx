99d3f22c76b668a3be7b8a89f8c777ab8b51d585
yersonargotev <yersonargotev@gmail.com>
2025-05-15 18:08:24 -0500

    feat: add svgl-react dependency and platform icons component
    - Added the @ridemountainpig/svgl-react package to package.json for SVG icon support.
    - Created a new PlatformIcon component to render platform-specific icons based on the selected theme, enhancing the visual representation of platforms in the application.

---
+++ src/components/icons/platforms.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+    AppleLight,
+    AppleDark,
+    Linux,
+    Windows,
+    Android,
+    Docker,
+    Kubernetes,
+} from '@ridemountainpig/svgl-react';
+import { Cloudy, Monitor, PcCase } from 'lucide-react';
+
+
+export type Platform = 'apple' | 'android' | 'windows' | 'linux' | 'docker' | 'kubernetes' | 'desktop' | 'self' | 'cloud';
+
+
+export interface PlatformIconsProps {
+    platform: Platform;
+    theme: 'light' | 'dark';
+}
+
+const Icons = {
+    apple: {
+        light: AppleLight,
+        dark: AppleDark,
+    },
+    android: {
+        light: Android,
+        dark: Android,
+    },
+    windows: {
+        light: Windows,
+        dark: Windows,
+    },
+    linux: {
+        light: Linux,
+        dark: Linux,
+    },
+    docker: {
+        light: Docker,
+        dark: Docker,
+    },
+    kubernetes: {
+        light: Kubernetes,
+        dark: Kubernetes,
+    },
+    desktop: {
+        light: Monitor,
+        dark: Monitor,
+    },
+    self: {
+        light: PcCase,
+        dark: PcCase,
+    },
+    cloud: {
+        light: Cloudy,
+        dark: Cloudy,
+    },
+};
+
+export const PlatformIcon = ({
+    platform,
+    theme,
+}: PlatformIconsProps) => {
+    const Icon = Icons[platform][theme];
+    return <Icon />;
+};
